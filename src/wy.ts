90e0915d011e20c527c7ef867cb6d1c2844ef920
yuzheng <34878821+zyTheGit@users.noreply.github.com>
2025-08-11 12:08:57 +0800

    feat(src)：文件调整

--- src/wy.ts
+++ src/wy.ts
@@ -3,7 +3,7 @@ import { sourceMap } from "./config";
 
 const { defaultConfig, pluginConfig, ...optMethods } = baseModules;
 
-defaultConfig.source = sourceMap.tencent;
+defaultConfig.source = sourceMap.netease;
 
 const pluginInstance: IPlugin.IPluginDefine = {
   platform: "阿九WY",
